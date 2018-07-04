36c537b38fc35c192b94fc1ffac908bf915fe1f7
Gregory Hart <gskhrvat@yahoo.com>
2018-07-04 08:55:52 -0600

    Starting app-8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+
+let baseUrl = "https://swapi.co/api/";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      people : {}
+    }
+  }
+
+  componentDidMount() {
+    let promise = axios.get(baseUrl + "/people/");
+    promise.then( (response) => {
+      console.log(response);
+    });
+    
+    
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div></div>
     );
   }
 }
